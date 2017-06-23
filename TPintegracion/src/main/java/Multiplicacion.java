729006ebf717d01b8af38e51a199fc6d45839f8f
dulce <marianeladulce@gmail.com>
2017-06-23 01:27:28 -0700

    Clase Multiplicacion

---
+++ TPintegracion/src/main/java/Multiplicacion.java
@@ -0,0 +1,14 @@
+
+public class Multiplicacion {private int num1;
+
+private int num2;
+
+public Multiplicacion(int n1, int n2) {
+    num1 = n1;
+    num2 = n2;
+}
+public int multiplicar() {
+    int resultado = num1 * num2;
+    return resultado;
+}
+}
