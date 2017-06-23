d12230c1a5394cfee8fa763c20a8de76f17ee9ce
dulce <marianeladulce@gmail.com>
2017-06-23 01:46:47 -0700

    crea clase Multi y delete Multiplicacion

--- TPintegracion/src/main/java/Multi.java
+++ TPintegracion/src/main/java/Multi.java
@@ -1,9 +1,9 @@
 
-public class Multiplicacion {private int num1;
+public class Multi {private int num1;
 
 private int num2;
 
-public Multiplicacion(int n1, int n2) {
+public Multi(int n1, int n2) {
     num1 = n1;
     num2 = n2;
 }
@@ -11,4 +11,5 @@ public int multiplicar() {
     int resultado = num1 * num2;
     return resultado;
 }
+
 }
