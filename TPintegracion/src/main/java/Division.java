76a6a3c25f92b04abe027b35f6f39fb919d94d5e
dulce <marianeladulce@gmail.com>
2017-06-23 02:40:33 -0700

    Clase Division

---
+++ TPintegracion/src/main/java/Division.java
@@ -0,0 +1,15 @@
+
+public class Division { private int num1;
+
+private int num2;
+
+public Division(int n1, int n2) {
+    num1 = n1;
+    num2 = n2;
+}
+public int dividir() {
+    int resultado = num1 / num2;
+    return resultado;
+}
+
+}
